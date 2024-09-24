be9433d6005f612a938378171a48a00d4fc4ff8a
Jhonatan Henrique <jhonatanhgq@hotmail.com>
2024-09-24 06:38:57 -0300

    bugs corrigitos no HTML e finalização do botão Estilo

---
+++ script.js
@@ -0,0 +1,23 @@
+const toggle = document.getElementById("toggle")
+const toggleText = document.getElementById("toggleText")
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    toggleText.textContent = "Claro"
+  } else {
+    toggleText.textContent = "Escuro"
+  }
+})
+
+const fileUpload = document.getElementById("file")
+const fileNameDisplay = document.getElementById("upload")
+
+fileUpload.addEventListener("change", function () {
+  const file = fileUpload.files[0]
+  if (file) {
+    fileNameDisplay.textContent = file.name
+    console.log("Tipo de arquivo:", file.type)
+  } else {
+    fileNameDisplay.textContent = "Nenhum arquivo selecionado"
+  }
+})
